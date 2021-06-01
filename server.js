982f50a496971f16e3cb10deff42de3f81600914
Karan Mishra <karan.m2704@gmail.com>
2021-06-01 09:18:06 +0530

    xyz

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ mongoose.connect(url, {
 
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname, 'client', 'build')))
+    app.use(express.static(path.join(__dirname, './client/build')))
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, './', 'client', 'build', 'index.html'))
     })
 }
 
